e350e97cf16faa155e5081b9e26bf57418f1167b
thomasreindl <reindlthomas@gmx.at>
2019-01-30 17:27:47 +0100

    initial commit

---
+++ Assets/Scripts/Basics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basics : MonoBehaviour {
+	
+	public Text overrideText;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hallo");
+		overrideText.text = "lala";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log("alive");
+	}
+}
